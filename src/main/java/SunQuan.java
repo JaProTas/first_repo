1730ed059552957732753b1cce5bafbbaedf9356
huangzhiming7 <13218924698@163.com>
2023-11-13 20:02:49 +0800

    孙权写代码 第一次

---
+++ src/main/java/SunQuan.java
@@ -0,0 +1,15 @@
+/**
+ * @author huangzhiming
+ * @date 2023/11/13
+ **/
+public class SunQuan {
+    public static void main(String[] args) {
+        System.out.println("孙权写代码");
+        System.out.println("孙权写代码");
+        System.out.println("孙权写代码");
+        System.out.println("孙权写代码");
+        System.out.println("孙权写代码");
+        System.out.println("孙权写代码");
+        System.out.println("孙权写代码");
+    }
+}
