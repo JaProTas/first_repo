4a125ab80aca748ec7dcb3fc06f2d89e9afa9ad4
huangzhiming7 <13218924698@163.com>
2023-11-13 20:05:26 +0800

    孙权写代码 第二次

---
+++ src/main/java/SunQuan2.java
@@ -0,0 +1,15 @@
+/**
+ * @author huangzhiming
+ * @date 2023/11/13
+ **/
+public class SunQuan2 {
+    public static void main(String[] args) {
+        System.out.println("孙权写代码22222222");
+        System.out.println("孙权写代码22222222");
+        System.out.println("孙权写代码22222222");
+        System.out.println("孙权写代码22222222");
+        System.out.println("孙权写代码22222222");
+        System.out.println("孙权写代码22222222");
+        System.out.println("孙权写代码22222222");
+    }
+}
